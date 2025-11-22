f7634f18dc446b12849f3b188ff0770cc29e324e
vishnuvv27 <vardhanvishnu520@gmail.com>
2025-11-22 16:21:16 +0530

    revenue calculator

---
+++ revenue_calculator/revenue.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	var revenue float64
+	var expenses float64
+	var taxRate float64
+
+	fmt.Print("Enter the revenue amount: ")
+	fmt.Scan(&revenue)
+	fmt.Print("Enter the expenses amount: ")
+	fmt.Scan(&expenses)
+	fmt.Print("Enter the taxRate: ")
+	fmt.Scan(&taxRate)
+
+	ebt := revenue - expenses
+	profit := ebt * (1 - taxRate/100)
+	ratio := ebt / profit
+
+	fmt.Println(ratio, ebt, profit)
+}
