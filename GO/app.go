4a19d7d0a5dc77486e9a5e17eeba2f4e8a162601
vishnuvv27 <vardhanvishnu520@gmail.com>
2025-11-22 16:06:19 +0530

    with input values

--- GO/app.go
+++ GO/app.go
@@ -6,11 +6,20 @@ import (
 )
 
 func main() {
-	var investmentAmount, years float64 = 1000, 10
-	expectedReturnRate := 5.5
+	const inflationRate = 7.0
+	var investmentAmount float64
+	var years float64
+	var expectedReturnRate float64
+	fmt.Print("Enter the investment amount: ")
+	fmt.Scan(&investmentAmount)
+	fmt.Print("Enter the years: ")
+	fmt.Scan(&years)
+	fmt.Print("Enter the expected Return Rate: ")
+	fmt.Scan(&expectedReturnRate)
 
 	futureValue := investmentAmount * math.Pow((1+expectedReturnRate/100), years)
-
+	futureRealValue := futureValue / math.Pow((1+inflationRate/100), years)
 	fmt.Println(futureValue)
+	fmt.Println(futureRealValue)
 
 }
